bd3b0af2e0c44dc00b229bb44902a0aa59bdfab1
RanCedric <dadatoantso@gmail.com>
2022-11-14 09:36:25 +0300

    Model V1

---
+++ Models/Societe.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Societe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'idsociete',
+        'nomsociete',
+        'reference',
+        'adresse',
+    ];
+}
